f833c808d010fe1172d092ebeaf34a705197022b
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro.local>
2018-01-08 21:24:06 -1000

    Completed Exercise 5

--- exercises.js
+++ exercises.js
@@ -114,7 +114,14 @@ console.log (mustBeTrue(true));
  * Console.log your result.
 */
 
-
+function bigBird(word){
+	if (word.length === 3){
+		return "Word to Big Bird!";
+	} else {
+		return "Go home";
+	}
+}
+console.log(bigBird("Cat"))
 
 /*
  * #6
